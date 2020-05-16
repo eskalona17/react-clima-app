c3881cfaa5b00e6165e36ae45753f36690323e3d
eskalona17 <eskalona_17@hotmail.com>
2020-05-16 17:09:54 +0200

    header component done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-   <h1>Hola mundo</h1>
+    <>
+    <Header titulo="Clima React App"/>
+   </>
   );
 }
 
