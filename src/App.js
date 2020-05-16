8b1f9f61bda3cb7133717e8df8b4820b0bfaa467
eskalona17 <eskalona_17@hotmail.com>
2020-05-16 21:18:37 +0200

    creating form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Formulario from './components/Formulario'
 
 function App() {
   return (
     <>
     <Header titulo="Clima React App"/>
+    <div className="contenedor-form">
+      <div className="container">
+        <div className="row">
+          <div className="col m6 s12">
+            <Formulario/>
+          </div>
+          <div className="col m6 s12">
+            2
+          </div>
+        </div>
+      </div>
+    </div>
    </>
   );
 }
