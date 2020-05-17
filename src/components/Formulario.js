75f52bdf1a589af57923544546ff4a2dd7062470
eskalona17 <eskalona_17@hotmail.com>
2020-05-17 11:57:10 +0200

    validating form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Formulario = () => {
+  //state del formulario
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  //extraer ciudad y pais
+  const { ciudad, pais } = busqueda;
+
+  //funcion que coloca los elementos en el state
+  const handleChange = (e) => {
+    //actualizar el state
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <form action="">
       <div className="input-field col s12">
-        <input type="text" name="ciudad" id="ciudad" />
-        <label htmlFor="ciudad">Ciudad</label>
+        <input
+          type="text"
+          name="ciudad"
+          id="ciudad"
+          value={ciudad}
+          onChange={handleChange}
+        />
+        <label htmlFor="ciudad">Ciudad: </label>
       </div>
       <div className="input-field col s12">
-        <select name="pais">
+        <select name="pais" value={pais} onChange={handleChange}>
           <option value="">-- Seleccione un país --</option>
           <option value="US">Estados Unidos</option>
           <option value="MX">México</option>
@@ -18,7 +41,7 @@ const Formulario = () => {
           <option value="ES">España</option>
           <option value="PE">Perú</option>
         </select>
-        <label htmlFor="ciudad">Ciudad</label>
+        <label htmlFor="ciudad">País:</label>
       </div>
     </form>
   );
